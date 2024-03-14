1292f0dd34530b69b6e3fcd448bdd9ae888f6304
aliyah-ym58 <aym58@txstate.edu>
2024-03-14 13:36:57 -0500

    Updated database.js file with new functionalities

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,24 +1,29 @@
 // database.js
 
+// Import the necessary MongoDB client
 const { MongoClient } = require('mongodb');
 
-const MONGO_URI = 'mongodb+srv://aym58:7RE8wxjhepp2EF69@cluster0.vvbzzw0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-const dbName = "<database>";
+// MongoDB Atlas connection details
+const MONGO_URI = 'mongodb+srv://aym58:7RE8wxjhepp2EF69@cluster0.vvbzzw0.mongodb.net/<database>';
+//const MONGO_URI = 'mongodb+srv://aym58:7RE8wxjhepp2EF69@cluster0.vvbzzw0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 let db;
 
+// Function to connect to the MongoDB database
 async function connectToDatabase() {
   const client = new MongoClient(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
   try {
+    // Connect to the MongoDB database
     await client.connect();
-    console.log('Connected to MongoDB');
-    db = client.db(dbName);
+    console.log('Connected to MongoDB Atlas');
+    db = client.db('<database>');
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
+    console.error('Error connecting to MongoDB Atlas:', error);
     process.exit(1);
   }
 }
 
+// Function to get a collection from the database
 function getCollection(collectionName) {
   if (!db) {
     throw new Error('Database connection not established');
@@ -26,6 +31,7 @@ function getCollection(collectionName) {
   return db.collection(collectionName);
 }
 
+//Function to store receipt data in the database
 async function storeReceipt(receiptData) {
   const collection = getCollection('receipts');
   try {
@@ -36,4 +42,10 @@ async function storeReceipt(receiptData) {
   }
 }
 
-module.exports = { connectToDatabase, getCollection, storeReceipt };
+async function findExampleDocument(){
+  const collection = getCollection('receipts');
+  const document = await collection.findONe();
+  console.log('Example Document:', document);
+}
+
+module.exports = { connectToDatabase, getCollection, storeReceipt, findExampleDocument };

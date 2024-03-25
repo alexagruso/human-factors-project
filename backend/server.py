4fd58df2cb14b91854d2c4d5ca12d34578d967f5
lgayler00 <86257307+lgayler00@users.noreply.github.com>
2024-03-25 14:43:00 -0500

    Item Classifier WIP
    added machine learning classification of individual items (WIP) and a dataset that assists us with it

--- backend/server.py
+++ backend/server.py
@@ -2,13 +2,38 @@ from flask import Flask, send_from_directory, request
 import random
 import base64
 import os
+import pandas as pd
 import json
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.svm import LinearSVC
 from azure.ai.formrecognizer import DocumentAnalysisClient
 from azure.core.credentials import AzureKeyCredential
 
 # set `<your-endpoint>` and `<your-key>` variables with the values from the Azure portal
-endpoint = "<your-endpoint>"
-key = "<your-key>"
+endpoint = "<endpoint>"
+key = "<key>"
+
+df = pd.read_csv('ourDataset.csv', usecols=['name', 'main_category'])
+
+# Feature extraction using TF-IDF
+vectorizer = TfidfVectorizer(stop_words='english')
+X = vectorizer.fit_transform(df['name'])
+
+# Target variable
+y = df['main_category']
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Initialize the Linear Support Vector Classifier
+classifier = LinearSVC()
+
+# Train the classifier
+classifier.fit(X_train, y_train)
+
+# Predictions on the test set
+#y_pred = classifier.predict(X_test)
 
 class SetEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -40,12 +65,12 @@ def ocrTool():
         if(receiptType):
             receiptDict['Category'] = 'PLACEHOLDER'
         else:
-            receipt['Category'] = 'N/A'
+            receiptDict['Category'] = 'N/A'
         merchant_name = receipt.fields.get("MerchantName")
         if merchant_name:
             receiptDict['MerchantName'] = merchant_name.value
         else:
-            receipt['MerchantName'] = 'N/A'
+            receiptDict['MerchantName'] = 'N/A'
         transaction_date = receipt.fields.get("TransactionDate")
         if transaction_date:
             receiptDict['TransactionDate'] = str(transaction_date.value)
@@ -54,10 +79,20 @@ def ocrTool():
         if receipt.fields.get("Items"):
             receiptDict['Items'] = []
             for idx, item in enumerate(receipt.fields.get("Items").value):
+                inferredResult = "null"
+                new_description = []
                 receiptItemsDict = {} #reset the items
                 item_description = item.value.get("Description")
                 if item_description:
                     receiptItemsDict['Name'] = item_description.value
+                    new_description.append(item_description.value)
+                    new_description_vectorized = vectorizer.transform(new_description)
+                    predicted_category = classifier.predict(new_description_vectorized)
+                    if(predicted_category[0] != 'Food' and predicted_category[0] != 'grocery & gourmet foods'): #two types of item expenses on receipts, food and shopping (yes many categories of shopping but they're all just shopping)
+                        inferredResult = 'Shopping/Entertainment'
+                    else:
+                        inferredResult = 'Food'
+                    receiptItemsDict['Item_Category'] = inferredResult
                 else:
                     receiptItemsDict['Name'] = 'N/A'
                 item_quantity = item.value.get("Quantity")

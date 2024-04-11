195db30ec17056ea68970683f084d3cbb960257d
AlexAgruso <agrusoalexander@gmail.com>
2024-04-11 14:20:27 -0500

    footer layout and styles

--- frontend/src/lib/components/footer.svelte
+++ frontend/src/lib/components/footer.svelte
@@ -1,9 +1,53 @@
-<footer>
-    <h1>Footer</h1>
+<script lang="ts">
+    let currentYear = new Date().getFullYear();
+</script>
+
+<footer class="row">
+    <div class="contact row">
+        <div class="copyright">
+            <span>Copyright &copy; {currentYear} ExpenseAid. All Rights Reserved.</span>
+        </div>
+        <div class="credits row">
+            <span>Created by</span>
+            <a href="/" target="_blank">Luke</a>
+            <span>,</span>
+            <a href="https://github.com/alexagruso" target="_blank">Alex</a>
+            <span>, and</span>
+            <a href="/" target="_blank">Alliyah</a>
+        </div>
+    </div>
 </footer>
 
 <style lang="scss">
     footer {
-        border: 1px solid black;
+        justify-content: center;
+
+        padding: 0.9rem;
+
+        background-color: $primary;
+
+        color: $white;
+    }
+
+    .contact {
+        gap: 2rem;
+    }
+
+    span {
+        margin-right: 0.25rem;
+
+        font-size: 1rem;
+    }
+
+    a {
+        transition: color 150ms;
+
+        color: $accent;
+        font-size: 1rem;
+        text-decoration: underline;
+
+        &:hover {
+            color: mix($accent, $white, 75%);
+        }
     }
 </style>

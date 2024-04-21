621400a2ae6c68f434702ab61bef292bfd375cd3
lgayler00 <86257307+lgayler00@users.noreply.github.com>
2024-04-21 09:17:02 -0500

    added line chart using new library
    added new library to package.json and implemented past 3 months of spending

--- frontend/src/routes/profile/statistics/+page.server.ts
+++ frontend/src/routes/profile/statistics/+page.server.ts
@@ -4,13 +4,20 @@ import { items, type Item } from "@lib/schemas/item";
 
 export const load: PageServerLoad = async ({ locals }) => {
     const userItems: Array<Item> = [];
+    let spending3Month : number = 0.00; //spending from 2 months ago
+    let spending2Month : number = 0.00; //spending from last month
+    let spendingMonthCurr : number = 0.00; //spending up to current day for current month
     
     let date: Date = new Date()
     let firstDay : Date = new Date(date.getFullYear(), date.getMonth(), 1); //retrieves first day of the current month
-    let lastDay : Date = new Date(date.getFullYear(), date.getMonth() + 1, 0); //retrives the last of the current month
+    let firstDayOf3MonthsAgo : Date = new Date(date.getFullYear(), date.getMonth()-2, 1);
+    let lastDayOf3MonthsAgo : Date = new Date(date.getFullYear(), date.getMonth()-1, 0);
+    let firstDayOf2MonthsAgo : Date = new Date(date.getFullYear(), date.getMonth()-1, 1);
+    let lastDayOf2MonthsAgo : Date = new Date(date.getFullYear(), date.getMonth(), 0); //retrieves first day of the current month
+
 
     try {
-        const foundReceipts: Receipt[] | undefined = await receipts.find({ userID: locals.userEmail, transactionDate: { $gte: firstDay, $lte: lastDay} }).lean();
+        const foundReceipts: Receipt[] | undefined = await receipts.find({ userID: locals.userEmail, transactionDate: { $gte: firstDay, $lte: date} }).lean();
 
         if (foundReceipts) {
             const object = JSON.parse(JSON.stringify(foundReceipts));
@@ -31,5 +38,46 @@ export const load: PageServerLoad = async ({ locals }) => {
         console.error(error);
     }
 
-    return { items: JSON.parse(JSON.stringify(userItems)) };
+    try{
+        const foundReceiptsMonth3: Receipt[] | undefined = await receipts.find({ userID: locals.userEmail, transactionDate: { $gte: firstDayOf3MonthsAgo, $lte: lastDayOf3MonthsAgo} }, 'grandTotal').lean();
+        if(foundReceiptsMonth3)
+        {
+            const object3Month = JSON.parse(JSON.stringify(foundReceiptsMonth3));
+            for(let i = 0; i < object3Month.length; i++)
+            {
+                spending3Month += object3Month[i].grandTotal;
+            }
+        }
+    } catch(error) {
+        console.error(error);
+    }
+    try{
+        const foundReceiptsMonth2: Receipt[] | undefined = await receipts.find({ userID: locals.userEmail, transactionDate: { $gte: firstDayOf2MonthsAgo, $lte: lastDayOf2MonthsAgo} }, 'grandTotal').lean();
+        if(foundReceiptsMonth2)
+        {
+            const object2Month = JSON.parse(JSON.stringify(foundReceiptsMonth2));
+            for(let i = 0; i < object2Month.length; i++)
+            {
+                spending2Month += object2Month[i].grandTotal;
+            }
+        }
+    } catch(error) {
+        console.error(error);
+    }
+    try{
+        const foundReceiptsMonthCurr: Receipt[] | undefined = await receipts.find({ userID: locals.userEmail, transactionDate: { $gte: firstDay, $lte: date} }, 'grandTotal').lean();
+        if(foundReceiptsMonthCurr)
+        {
+            const objectMonthCurr = JSON.parse(JSON.stringify(foundReceiptsMonthCurr));
+            for(let i = 0; i < objectMonthCurr.length; i++)
+            {
+                spendingMonthCurr += objectMonthCurr[i].grandTotal;
+            }
+        }
+    } catch(error) {
+        console.error(error);
+    }
+
+    return { items: JSON.parse(JSON.stringify(userItems)), month3Value: spending3Month, month2Value: spending2Month, monthCurrValue: spendingMonthCurr};
 };
+

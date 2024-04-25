7c65fce8e0f9504b3ee8e2cf1e2f1c2e2e814feb
AlexAgruso <agrusoalexander@gmail.com>
2024-04-25 00:32:56 -0500

    resolve form action issue

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,25 +1,28 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-auto";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
-		alias: {
-			"@components": "./src/lib/components",
-			"@lib": "./src/lib",
-		}
-	},
-	onwarn: (warning, handler) => {
-		const { code } = warning;
+    kit: {
+        adapter: adapter(),
+        alias: {
+            "@components": "./src/lib/components",
+            "@lib": "./src/lib",
+        },
+        csrf: {
+            checkOrigin: false,
+        },
+    },
+    onwarn: (warning, handler) => {
+        const { code } = warning;
 
-		if (code === 'css-unused-selector') {
-			return;
-		}
+        if (code === "css-unused-selector") {
+            return;
+        }
 
-		handler(warning);
-	},
-	preprocess: vitePreprocess(),
+        handler(warning);
+    },
+    preprocess: vitePreprocess(),
 };
 
 export default config;
